666490c8760141b7971100868c8022a9ccdea7aa
Ourouk <spelgattiandrea@gmail.com>
2023-06-02 17:21:03 +0200

    Split Ftp and http Server as too different process

---
+++ src/main/java/com/hepl/ftp_server.java
@@ -0,0 +1,12 @@
+package com.hepl;
+
+import com.hepl.customFtpServer.ftpMain;
+
+public class ftp_server {
+
+    public static void main(String[] args) {
+        ftpMain ftp_server = new ftpMain();
+        ftp_server.run();
+    }
+}
+
